e7923d836c87bae86debe358512aa854124296a8
Joana Miguel Gómez <jmiguel@ucol.mx>
2022-02-17 18:22:12 -0600

    contador funcionando, solo segundos

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import React, {useState, useEfect} from "react";
+import React, {useState, useEffect} from "react";
 import {Text, View, Button} from 'react-native';
 
 const style = {
   fontSize: 30,
 };
 
-const NuevoComponente = ({title}) => {
-  return <Text style={style}>{title}</Text>
+const NuevoComponente = ({text}) => {
+  return <Text style={style}>{text}</Text>
 };
 
 const App = () => {
+  const [segundos, setSegundos] = useState(0);
 
+  useEffect(() =>{
+    const temporizador = setInterval(()=>{
+      setSegundos(segundos => segundos + 1);
+    },1000);
+    return () => clearInterval(temporizador);
+  }, []);
+  
   return (
-    <View>
-      <NuevoComponente title ="contador:"/>
+    
 
+    <View>
+      <NuevoComponente text = ":"/>
+      <NuevoComponente text ={segundos}/>
     </View>
+    
   );
 };
 export default App;
